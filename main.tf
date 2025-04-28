4eacbfd59c7e1def4e830bae2ca95c953d8c8880
RaflyAdiyasa <rafly271008@gmail.com>
2025-04-28 19:34:49 +0700

    add some variable

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
   credentials = file(var.gcp_svc_key)
 }
 
@@ -8,7 +8,7 @@ resource "google_compute_instance" "vm_sql-tf" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
-  tags = [var.tags.1 , var.tags.2, var.tags.3]
+  tags         = [var.tags.1, var.tags.2, var.tags.3]
 
   can_ip_forward      = false
   deletion_protection = false
@@ -18,7 +18,7 @@ resource "google_compute_instance" "vm_sql-tf" {
 
     auto_delete = true
     device_name = "disk-tf"
-    
+
     initialize_params {
       image = var.image
       size  = var.disk_size
@@ -28,7 +28,7 @@ resource "google_compute_instance" "vm_sql-tf" {
     mode = "READ_WRITE"
   }
 
-    labels = {
+  labels = {
     goog-ec-src = "vm_add-tf"
   }
 
@@ -37,17 +37,17 @@ resource "google_compute_instance" "vm_sql-tf" {
     access_config {
       network_tier = "PREMIUM"
     }
-    
+
   }
 
- 
+
 
   metadata_startup_script = file(var.metadata_startup_script)
 }
 
 
 output "ip-address" {
-  value = google_compute_instance.vm_sql-tf.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.vm_sql-tf.network_interface[0].access_config[0].nat_ip
   description = "IP Address of the VM instance"
-  
+
 }
